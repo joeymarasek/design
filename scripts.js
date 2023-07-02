5522a1f56981d4791b1bed222b1f6d791f752ff3
joeymara <joeymarasek@gmail.com>
2023-07-02 13:54:01 +0200

    updated email sending

--- scripts.js
+++ scripts.js
@@ -88,30 +88,3 @@ new TypeIt("#heroType", {
     var skills = new SkillsBar(".skillbar-bar");
   });
 })();
-
-/*************************
-    Emails
-*************************/
-
-function ajaxpost () {
-  // (A) GET FORM DATA
-  var data = new FormData(document.getElementById("contactForm"));
- 
-  // (B) AJAX REQUEST
-  // (B1) POST DATA TO SERVER, RETURN RESPONSE AS TEXT
-  fetch("1c-server.html", { method:"POST", body:data })
-  .then(res => res.text())
- 
-  // (B2) SHOW MESSAGE ON SERVER RESPONSE
-  .then(response => {
-    console.log(response);
-    if (response == "OK") { alert("SUCCESSFUL!"); }
-    else { alert("FAILURE!"); }
-  })
- 
-  // (B3) OPTIONAL - HANDLE FETCH ERROR
-  .catch(err => console.error(err));
- 
-  // (C) PREVENT FORM SUBMIT
-  return false;
-}
